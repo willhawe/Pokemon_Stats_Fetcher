58360a6d0fca72d61fa71a6d54f3a650dc84b094
willhawe <williamhawe8888@gmail.com>
2024-01-10 16:07:16 +0000

    Ran python Black
    added a few empty lines

--- pokemon.py
+++ pokemon.py
@@ -5,6 +5,7 @@ import sqlite3
 POKEMON_API = "https://pokeapi.co/api/v2/pokemon/"
 DB_FILE = "pokemon_stats.db"
 
+
 def get_pokemon_details_from_api(pokemon_name):
     url = POKEMON_API + pokemon_name
     try:
@@ -14,27 +15,40 @@ def get_pokemon_details_from_api(pokemon_name):
         sys.exit(1)
 
     inbound_dict = {
-        'id': api_response.get('id'),
-        'name': api_response.get('name'),
-        'height': api_response.get('height'),
-        'weight': api_response.get('weight')
+        "id": api_response.get("id"),
+        "name": api_response.get("name"),
+        "height": api_response.get("height"),
+        "weight": api_response.get("weight"),
     }
     return inbound_dict
+
 
 def create_table(cursor):
     command_create_table = """CREATE TABLE IF NOT EXISTS
     store(id INTEGER PRIMARY KEY, name TEXT, height INTEGER, weight INTEGER)"""
     cursor.execute(command_create_table)
 
+
 def insert_into_table(cursor, pokemon_details):
     command_select = "SELECT * FROM store WHERE name = ?"
-    cursor.execute(command_select, (pokemon_details['name'].lower(),))
+    cursor.execute(command_select, (pokemon_details["name"].lower(),))
     result = cursor.fetchone()
 
     if not result:
-        command_insert = "INSERT INTO store (id, name, height, weight) VALUES (?, ?, ?, ?)"
-        cursor.execute(command_insert, (pokemon_details['id'], pokemon_details['name'], pokemon_details['height'], pokemon_details['weight']))
+        command_insert = (
+            "INSERT INTO store (id, name, height, weight) VALUES (?, ?, ?, ?)"
+        )
+        cursor.execute(
+            command_insert,
+            (
+                pokemon_details["id"],
+                pokemon_details["name"],
+                pokemon_details["height"],
+                pokemon_details["weight"],
+            ),
+        )
         cursor.connection.commit()
+
 
 def print_pokemon_details(result):
     if result:
@@ -44,6 +58,7 @@ def print_pokemon_details(result):
         print(f"Height: {result[2]}")
         print(f"Weight: {result[3]}")
         print("=======================")
+
 
 def main():
     if len(sys.argv) != 2:
@@ -62,16 +77,17 @@ def main():
 
         command_select = "SELECT * FROM store WHERE name = ?"
         cursor.execute(command_select, (pokemon_name,))
-        result = cursor.fetchone()                                                  
-                                                                                    
+        result = cursor.fetchone()
+
         print_pokemon_details(result)
 
     except sqlite3.Error as e:
         print(f"SQLite error: {e}")
 
-    finally:                                                                       
+    finally:
         if connection:
             connection.close()
+
 
 if __name__ == "__main__":
     main()
